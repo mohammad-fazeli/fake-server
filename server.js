e022eb48d4a740dd986c9d578f4e7eedd280a1f9
mohammad-fazeli <moohammadfazeli@gmail.com>
2021-12-04 11:51:17 +0330

    update

--- server.js
+++ server.js
@@ -11,13 +11,9 @@ const bookpproducts = jsonServer.router("./db/bookproducts.json");
 const clothing = jsonServer.router("./db/clothing.json");
 const clothingproducts = jsonServer.router("./db/clothingproducts.json");
 const menshoes = jsonServer.router("./db/menshoes.json");
-const menshoesproducts = jsonServer.router("./db/clothingproducts.json");
 const menswear = jsonServer.router("./db/menswear.json");
-const menswearproducts = jsonServer.router("./db/clothingproducts.json");
 const womenshoes = jsonServer.router("./db/womenshoes.json");
-const womenshoesproducts = jsonServer.router("./db/clothingproducts.json");
 const womenswear = jsonServer.router("./db/womenswear.json");
-const womenswearproducts = jsonServer.router("./db/clothingproducts.json");
 const posts = jsonServer.router("./db/posts.json");
 const products = jsonServer.router("./db/products.json");
 const db = jsonServer.router("./db/db.json");
@@ -38,21 +34,17 @@ server.use("/laptop", laptop);
 server.use("/laptopproducts", laptopproducts);
 
 server.use("/book", book);
-server.use("/bookpproducts", bookpproducts);
+server.use("/bookproducts", bookpproducts);
 
 server.use("/clothing", clothing);
 server.use("/clothingproducts", clothingproducts);
 
 server.use("/menshoes", menshoes);
-server.use("/menshoesproducts", menshoesproducts);
 
 server.use("/menswear", menswear);
-server.use("/menswearproducts", menswearproducts);
 
 server.use("/womenshoes", womenshoes);
-server.use("/womenshoesproducts", womenshoesproducts);
 
 server.use("/womenswear", womenswear);
-server.use("/womenswearproducts", womenswearproducts);
 
 server.listen(port);
